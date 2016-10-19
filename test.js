7d992467fc2b7246ba68b24dc1a700c282f1996e
Lu Nelson <lunelson@gmail.com>
2016-10-19 15:53:35 +0200

    lots of new tests

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var path = require('path');
 var sassTrue = require('sass-true');
-var sassFile = path.join(__dirname, 'test/_index.scss');
+var sassFile = path.join(__dirname, 'test.scss');
 sassTrue.runSass({file: sassFile}, describe, it);
